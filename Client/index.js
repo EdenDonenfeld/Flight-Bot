05f58b6e30143b4eedddb6f033204794248df447
EdenDonenfeld <edendonenfeld@gmail.com>
2024-01-14 18:30:31 +0200

    Update Client side design

--- Client/index.js
+++ Client/index.js
@@ -1,9 +1,26 @@
 async function onSendMessage() {
-  const input = document.getElementById('messageInput')
+  const input = document.getElementById('messageInput');
+  
   if (!input.value) {
     return;
   }
-  const response = await fetch(`http://localhost:3000/api/flightbot/${input.value}`, {
+
+  var val = input.value;
+  console.log(val);
+
+  if (val.trim() != '') {
+    // Adding a message
+    var chatMessages = document.getElementById("chat-messages");
+    var newMessage = document.createElement('div');
+    newMessage.className = "message";
+    newMessage.textContent = val;
+    chatMessages.appendChild(newMessage);
+    input.value = "";
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  }
+  
+
+  const response = await fetch(`http://localhost:3000/api/flightbot/${val}`, {
     method: 'POST'
   });
   const message = await response.text();
