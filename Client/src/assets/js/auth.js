e59930972978ff4733a888393dd734aec58a3ec5
Royi Reznik <royi.reznik@gmail.com>
2024-04-12 15:09:25 +0300

    Separated LogIn and SignUp from dashboard, Create each FINAL Css and Js. handled errors in signUp and show to the users by appropriate message, make user jump to login page after signUp successfully.

--- Client/src/assets/js/auth.js
+++ Client/src/assets/js/auth.js
@@ -2,20 +2,10 @@ import { auth } from './firebase.js';
 const { signInWithEmailAndPassword, onAuthStateChanged, createUserWithEmailAndPassword, signOut } = window.firebase;
 
 function signUp(email, password) {
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        // Signed up successfully
-        var user = userCredential.user;
-    })
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // Handle errors
-    });
+    return createUserWithEmailAndPassword(auth, email, password)
 }
 
 function signIn(email, password) {
-    console.log(auth)
     return signInWithEmailAndPassword(auth, email, password)
 }
 
