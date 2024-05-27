480ea3503682a6c926f1d9ac1bdfa7234f7e389c
EdenDonenfeld <edendonenfeld@gmail.com>
2024-05-27 12:29:03 +0300

    Fixed bugs on server

--- Client/index.js
+++ Client/index.js
@@ -24,16 +24,38 @@ async function onSendMessage() {
     chatMessages.scrollTop = chatMessages.scrollHeight;
   }
 
-  const response = await fetch(`/api/flightbot/${encodeURIComponent(val)}`, {
-    method: 'POST'
-  });
-  const message = await response.text();
+  console.log("Message", val);
 
-  // Adding a message from server
-  var chatMessages = document.getElementById("chat-messages");
-  var newMessage = document.createElement('div');
-  newMessage.className = "message-back";
-  newMessage.textContent = message;
-  chatMessages.appendChild(newMessage);
-  chatMessages.scrollTop = chatMessages.scrollHeight;
+  try {
+    const response = await fetch(`/api/flightbot`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message: val })
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    const message = data.response;
+
+    // Adding a message from server
+    let chatMessages = document.getElementById("chat-messages");
+    let newMessage = document.createElement('div');
+    newMessage.className = "message-back";
+    newMessage.textContent = message;
+    chatMessages.appendChild(newMessage);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  }
+  catch (error) {
+    console.error('Error:', error);
+  }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('submitButton').addEventListener('click', onSendMessage);
+  document.getElementById('messageInput').addEventListener('keydown', handleKeyDown);
+})
--- Server/app.py
+++ Server/app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 def create_app():
-    app = Flask(__name__, static_folder='../client', template_folder='../client')
+    app = Flask(__name__, static_folder='../Client', template_folder='../Client')
 
     @app.route('/chat')
     def chat():
         return render_template('index.html')
 
-    @app.route('/api/flightbot/<message>', methods=['POST'])
-    def flightbot(message):
+    @app.route('/api/flightbot', methods=['POST'])
+    def flightbot():
+        data = request.get_json()
+        message = data.get('message', '')
         response = message + " !!!"
-        return response
+        print(response)
+        return jsonify({'response': response})
 
     return app
