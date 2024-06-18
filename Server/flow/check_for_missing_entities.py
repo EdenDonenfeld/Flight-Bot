c386cf4f5db7b9f1725a30bf0543212bc6f900a6
Nizan Dvir <nizandvir2005@gmail.com>
2024-06-18 21:21:04 +0300

    notes for roye

--- Server/flow/check_for_missing_entities.py
+++ Server/flow/check_for_missing_entities.py
@@ -1,9 +1,14 @@
 def check_for_missing(Entities, Predicted_label):
+    missing = [1,1,1,1]
     # label 0 - order a ticket
     # label 1 - refund a ticket
     # label 2 - check the status of a ticket
     # label 3 - change the date of a ticket
     # label 4 - change the destination of a ticket
+
+
     # label 5 - know the weather of a destination
     # label 6 - know what is allowed in the flight
-    return 1
+
+    # entities = {"Origin": None, "Destination": None, "Date": None, "Date2": None}
+    return missing
